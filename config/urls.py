14db34f86cf5927a1a2f515c4e3b3603351df25a
Ranagb <ranaeditz@gmail.com>
2026-06-30 13:15:46 +0500

    final working version

--- config/urls.py
+++ config/urls.py
@@ -5,13 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # 🛍️ Product app (home is inside products)
     path('', include('products.urls')),
-
-    # 🛒 Cart
     path('cart/', include('cart.urls')),
 ]
 
-# 👇 MEDIA FILES (images fix)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
