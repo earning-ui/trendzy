80ce46d9755d01206a996ff0192dd56bcb3f1fe8
Ranagb <ranaeditz@gmail.com>
2026-06-30 13:01:24 +0500

    fix urls and media image loading

--- config/urls.py
+++ config/urls.py
@@ -2,26 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # 🏠 Home page (ONLY HERE)
-    path('', views.home, name='home'),
-
-    # 🛍️ Product app
+    # 🛍️ Product app (home is inside products)
     path('', include('products.urls')),
 
     # 🛒 Cart
     path('cart/', include('cart.urls')),
-
-    # 👤 Accounts
-    path('accounts/', include('accounts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# 👇 MEDIA FILES (images fix)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
